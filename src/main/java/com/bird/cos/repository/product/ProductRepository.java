14529c80ddef9c31961d7d8224b68d9ff4238870
onuyyy <qazxsw9026@naver.com>
2025-09-19 11:55:42 +0900

    feat : Admin 상품 등록/브랜드 등록 기능

--- src/main/java/com/bird/cos/repository/product/ProductRepository.java
+++ src/main/java/com/bird/cos/repository/product/ProductRepository.java
@@ -53,20 +53,24 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     // 제품명으로 검색
     Page<Product> findProductsByProductTitleContainingIgnoreCase(String productTitle, Pageable pageable);
-
+    
     // 브랜드명으로 검색
     @Query("SELECT p FROM Product p WHERE p.brand.brandName LIKE %:brandName%")
     Page<Product> findProductsByBrandNameContainingIgnoreCase(@Param("brandName") String brandName, Pageable pageable);
-
+    
     // 카테고리명으로 검색
     @Query("SELECT p FROM Product p WHERE p.productCategory.categoryName LIKE %:categoryName%")
     Page<Product> findProductsByCategoryNameContainingIgnoreCase(@Param("categoryName") String categoryName, Pageable pageable);
-
+    
     // 상품 상태로 검색
     @Query("SELECT p FROM Product p WHERE p.productStatusCode.codeName LIKE %:status%")
     Page<Product> findProductsByStatusContainingIgnoreCase(@Param("status") String status, Pageable pageable);
-
+    
     // 색상으로 검색
     Page<Product> findProductsByProductColorContainingIgnoreCase(String productColor, Pageable pageable);
+
+    // 같은 브랜드, 같은 이름 존재하는지 검색
+    Boolean existsByProductTitleAndBrand_BrandId(String productTitle, Long brandId);
+}
 
 }
